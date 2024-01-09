72f240f5ceb112ff1c9611fee148569b08d45138
Gorkem Gocer <gorkemgocer7@gmail.com>
2024-01-09 16:55:38 +0300

    oauth configured

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,6 @@
 import Credentials from "next-auth/providers/credentials";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
 import bcrypt from "bcryptjs";
 
@@ -7,6 +9,14 @@ import { getUserByEmail } from "@/lib/user";
 
 export default {
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
       credentials: {
         email: { type: "text" },
--- components/auth/socials.tsx
+++ components/auth/socials.tsx
@@ -1,17 +1,39 @@
 "use client";
 import React from "react";
+import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "../ui/button";
+import { DEFAULT_REDIRECT } from "@/routes";
+
 type Props = {};
 
 const Socials = (props: Props) => {
+  const onClickHandler = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_REDIRECT,
+    });
+  };
   return (
     <div className="w-full flex items-center gap-2 ">
-      <Button variant={"secondary"} className="w-full " size={"lg"}>
+      <Button
+        variant={"secondary"}
+        className="w-full "
+        size={"lg"}
+        onClick={() => {
+          return onClickHandler("google");
+        }}
+      >
         <FcGoogle className="h-6 w-6" />
       </Button>
-      <Button variant={"secondary"} className="w-full " size={"lg"}>
+      <Button
+        variant={"secondary"}
+        className="w-full "
+        size={"lg"}
+        onClick={() => {
+          return onClickHandler("github");
+        }}
+      >
         <FaGithub className="h-6 w-6" />
       </Button>
     </div>
